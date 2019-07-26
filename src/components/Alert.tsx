7cd6e881d82115bc76c77a61fe4bd667addaff7d
Andries Smit <Andries.Smit@mendix.com>
2019-07-26 11:31:13 +0200

    Add alert new style class

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,16 +1,15 @@
 import { FunctionComponent, createElement } from "react";
-import * as classNames from "classNames";
-
+import classNames from "classnames";
 export interface AlertProps {
     id?: string;
     alertStyle?: "default" | "primary" | "success" | "info" | "warning" | "danger";
     className?: string;
 }
-
 export const Alert: FunctionComponent<AlertProps> = ({ alertStyle, className, children, id }) =>
-    children
-        ? <div id={id} className={classNames(`alert alert-${alertStyle}`, className) }>{children}</div>
-        : null;
-
+    children ? (
+        <div id={id} className={classNames(`alert alert-${alertStyle} mx-validation-message`, className)}>
+            {children}
+        </div>
+    ) : null;
 Alert.displayName = "Alert";
 Alert.defaultProps = { alertStyle: "danger" };
